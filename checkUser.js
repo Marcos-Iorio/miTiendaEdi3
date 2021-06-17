31c93d0ff2d5385a8e24e5cf93062faf694c1841
Marcos Iorio <marcossiorio@gmail.com>
2021-06-17 19:28:10 -0300

    cambios en la ruta

--- checkUser.js
+++ checkUser.js
@@ -1,19 +1,23 @@
-const RutaServer = "https://edi-iorio-back.herokuapp.com/"
+const RutaServer = "http://localhost:8080";
 
 function checkData(valor){
     document.getElementById("resultado").innerHTML = valor;
 }
 
-function iniciarSesion(rutaServer) {
 
+
+function iniciarSesion() {
+    datos ="/login" + "/" + document.getElementById('nameL').value + "/" + document.getElementById('passL').value;
+    console.log(datos);
     //declaro el objeto
     var xmlhttp = new XMLHttpRequest();
-    var data = new FormData();
+   /*  var data = new FormData();
     data.append("Nombre", $("nameL").value);
-    data.append("Contraseña", $('passL').value);
+    data.append("Contraseña", $('passL').value); */
+    
 
     // indico hacia donde va el mensaje
-    xmlhttp.open("POST", RutaServer + rutaServer, true);
+    xmlhttp.open("GET", RutaServer  + datos, true);
     //seteo el evento
     xmlhttp.onreadystatechange = function () {
         //Veo si llego la respuesta del servidor
@@ -21,14 +25,14 @@ function iniciarSesion(rutaServer) {
             //Reviso si la respuesta es correcta
             if (xmlhttp.status == 200) {
                 alert(xmlhttp.responseText);
-                alert("Datos correctos!");
+                document.getElementById("resultado").innerHTML = xmlhttp.responseText;
             }
             else {
                 alert("ocurrio un error");
             }
         }
     }
-    xmlhttp.setRequestHeader('enctype', 'multipart/form-data');
+    /* xmlhttp.setRequestHeader('enctype', 'multipart/form-data'); */
     //envio el mensaje    
-    xmlhttp.send(data);
+    xmlhttp.send();
 }
