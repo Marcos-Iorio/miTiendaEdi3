6dd73d65e66c41e0f7131fb44880cd58ce8a29b8
Marcos Iorio <marcossiorio@gmail.com>
2021-06-19 16:04:29 -0300

    Agregando remote sql y POST

--- checkUser.js
+++ checkUser.js
@@ -18,7 +18,7 @@ function enviarMensajeAlServidor(RutaServer) {
             //Reviso si la respuesta es correcta
             if (xmlhttp.status == 200) {
               /*   funcionARealizar(xmlhttp.responseText); */
-                document.getElementById("resultado").innerHTML = xmlhttp.responseText;
+                document.getElementById("login").innerHTML = xmlhttp.responseText;
             }
             else {
                 alert("ocurrio un error");
@@ -31,6 +31,43 @@ function enviarMensajeAlServidor(RutaServer) {
     var obje = new FormData();
     obje.append("Nombre", document.getElementById("nameL").value);
     obje.append("Contraseña", document.getElementById('passL').value);
+    //envio el mensaje    
+    xmlhttp.send(obje);
+
+}   
+
+function registro(){
+    enviarRegistro(RutaServer);
+}
+
+function enviarRegistro(RutaServer) {
+    datos ="/login/"
+    //declaro el objeto
+    var xmlhttp = new XMLHttpRequest();
+
+    // indico hacia donde va el mensaje
+    xmlhttp.open("POST", RutaServer + datos, true);
+    //seteo el evento
+    xmlhttp.onreadystatechange = function () {
+        //Veo si llego la respuesta del servidor
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+            //Reviso si la respuesta es correcta
+            if (xmlhttp.status == 200) {
+              /*   funcionARealizar(xmlhttp.responseText); */
+                document.getElementById("registro").innerHTML = xmlhttp.responseText;
+            }
+            else {
+                alert("ocurrio un error");
+            }
+        }
+    }
+
+    xmlhttp.setRequestHeader('enctype', 'multipart/form-data');
+    //envio el mensaje    
+    var obje = new FormData();
+    obje.append("Nombre", document.getElementById("username").value);
+    obje.append("Contraseña", document.getElementById('passwordRe').value);
+    obje.append("Mail", document.getElementById('mail').value);
     //envio el mensaje    
     xmlhttp.send(obje);
 
