20cd8ebaca993bbe6dcdeba616070546784a53b2
Marcos Iorio <marcossiorio@gmail.com>
2021-06-24 12:20:02 -0300

    filtrando productos

--- productos.js
+++ productos.js
@@ -146,7 +146,7 @@ function retornarCategoria(RutaServer, funcionARealizar){
     xmlhttp.setRequestHeader('enctype', 'multipart/form-data');
     //envio el mensaje    
     var obje = new FormData();
-    obje.append("Categoria", document.getElementById("categoria").innerHTML);
+    obje.append("Categoria", document.getElementById("categoria").value);
     //envio el mensaje    
     xmlhttp.send(obje);
 
